2ae92369fb53cf5117c9984d74ec6f70198bf6d7
Alexander <akouthoofd@salesforce.com>
2020-06-03 18:55:48 -0700

    Make archive idempotent
    - events that have been restored in the past will not be archived a second time

--- cantor-misc/src/main/java/com/salesforce/cantor/misc/archivable/EventsArchiver.java
+++ cantor-misc/src/main/java/com/salesforce/cantor/misc/archivable/EventsArchiver.java
@@ -25,7 +25,25 @@ public interface EventsArchiver {
                         final long endTimestampMillis);
 
     /**
+     * Will retrieve and archive all events before the provided timestamp.
+     * <br><br>
+     * {@code archive()} may store events past {@code endTimestampMillis} as chunking of the data into buckets may be used.
+     * <br><br>
+     * It will depend on the implementation.
+     */
+    default void archive(final Events events,
+                 final String namespace,
+                 final long endTimestampMillis) throws IOException {
+        archive(events, namespace, 0, endTimestampMillis, null, null);
+    }
+
+    /**
      * Will retrieve and archive all events using these given parameters and load this into the destination.
+     * <br><br>
+     * {@code archive()} may store events before {@code startTimestampMillis} and past {@code endTimestampMillis}
+     * as chunking of the data into buckets may be used.
+     * <br><br>
+     * It will depend on the implementation.
      */
     void archive(final Events events,
                  final String namespace,
@@ -33,18 +51,6 @@ public interface EventsArchiver {
                  final long endTimestampMillis,
                  final Map<String, String> metadataQuery,
                  final Map<String, String> dimensionsQuery) throws IOException;
-
-    /**
-     * Will retrieve and archive all events before the provided timestamp.
-     * <br><br>
-     * {@code archive()} may not necessarily store every event up to the {@code endTimestampMillis} as chunking of the
-     * data into buckets may be used.
-     * <br><br>
-     * It will depend on the implementation.
-     */
-    void archive(final Events events,
-                 final String namespace,
-                 final long endTimestampMillis) throws IOException;
 
     /**
      * Will retrieve all archived chunks between the provided timestamps and load them back into Cantor.
